5fb1bc163ecf24f6df3029edfca13d2983d71070
Simon Pfreundschuh <simon.pfreundschuh@colostate.edu>
2024-05-08 14:57:48 -0600

    Add missing module.

---
+++ speed/logging.py
@@ -0,0 +1,13 @@
+"""
+speed.logging
+=============
+
+Sets the RichHandler as logging handler.
+"""
+import logging
+from rich.logging import RichHandler
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
